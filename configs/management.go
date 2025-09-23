7b463b7c48bbbccabc126a3519669bde1fb27e59
sauce-git <dadad02100@gmail.com>
2025-09-23 22:16:00 +0900

    refactor: delete kek

--- configs/management.go
+++ configs/management.go
@@ -2,7 +2,6 @@ package configs
 
 type ManagementConfig struct {
 	Env      string         `env:"ENV" envDefault:"prod" validate:"oneof=dev staging prod"`
-	KEK      string         `env:"KEK" validate:"required,len=64,hexadecimal"` // 256-bit hex-encoded key (64 chars)
 	Server   ServerConfig   `envPrefix:""`
 	Postgres PostgresConfig `envPrefix:""`
 	Redis    RedisConfig    `envPrefix:""`
